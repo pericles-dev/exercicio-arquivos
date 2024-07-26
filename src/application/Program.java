33e0b8fcec281876ca8761dec687a4794ec436e3
Péricles Santos <pericles.programador@gmail.com>
2024-07-26 09:16:57 -0300

    Inicio do programa principal, e leitura de dados csv

---
+++ src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		List<Product> lista = new ArrayList<>();
+		
+		System.out.print("Digite o caminho do arquivo: ");
+		String caminhoArquivo = sc.nextLine();
+		
+		File arquivo = new File(caminhoArquivo);
+		String caminhoRaiz = arquivo.getParent();
+		
+		//boolean pasta = new File(caminhoRaiz + "/out").mkdir();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo))){
+
+			String linha = br.readLine();
+			
+			while (linha != null) {
+				String[] dadosProduto = linha.split(",");
+				
+				String name = dadosProduto[0];
+				Double price = Double.parseDouble(dadosProduto[1]);
+				Integer quantity = Integer.parseInt(dadosProduto[2]);
+				
+				lista.add(new Product(name, price, quantity));
+				
+				linha = br.readLine();
+			}
+			
+		} catch (IOException e) {
+			System.out.println("Erro ao ler arquivo: " + e.getMessage());
+		}
+	}
+
+}
